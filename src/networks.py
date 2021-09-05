9615b1c1b085eab5fe485e3c946037644b9cda45
Han jun <hhjjhh0425@naver.com>
2021-09-06 01:20:21 +0900

    model network

--- src/networks.py
+++ src/networks.py
@@ -1,4 +1,3 @@
-from tensorflow.python.keras.backend import dropout
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,17 +8,25 @@ from fk_torch import FK
 
 
 class Gru_model(nn.Module):
-    def __init__(self, hidden_size, num_layers, kp):
+    def __init__(self, hidden_size, num_layers, kp=0.7):
         super(Gru_model, self).__init__()
         self.hidden_size=hidden_size
         self.num_layers=num_layers
-        self.gru=nn.GRU(70, hidden_size, num_layers, dropout=1-kp, batch_first=True)
+        self.cells=[]
+        self.dropout=[]
+        self.cells.append(nn.GRUCell(70, hidden_size)) 
+        for i in range(num_layers-1):
+            self.cells.append(nn.GRUCell(hidden_size, hidden_size))  
+        for i in range(num_layers):
+            self.dropout.append(nn.Dropout(p=1-kp))     
     
-    def forward(self, input, hidden):
-        return self.gru(input, hidden)
-    
-    def initHidden(self, batch_size):
-        return torch.zeros(self.num_layers, batch_size, self.hidden_size, device=device)
+    def forward(self, x, initial_state):
+        hidden=[]
+        for i in range(self.num_layers):
+            x=self.dropout[i](x)
+            x=self.cells[i](x, initial_state[i])
+            hidden.append(x)
+        return hidden
         
 class Generator(nn.Module):
     def __init__(self, hidden_size, num_layers, n_joints, batch_size, kp):
@@ -41,23 +48,29 @@ class Generator(nn.Module):
         a_global = []
         a_quats = []
 
-        statesA_AB = self.enc_gru.initHidden(self.batch_size)
-        statesB_AB = self.dec_gru.initHidden(self.batch_size)
-        statesA_BA = self.dec_gru.initHidden(self.batch_size)
-        statesB_BA = self.enc_gru.initHidden(self.batch_size)
+        statesA_AB = []
+        statesB_AB = []
+        statesA_BA = []
+        statesB_BA = []
+        
+        for _ in self.num_layers:
+            statesA_AB += [torch.zeros((self.batch_size, self.hidden_size))]
+            statesB_AB += [torch.zeros((self.batch_size, self.hidden_size))]
+            statesA_BA += [torch.zeros((self.batch_size, self.hidden_size))]
+            statesB_BA += [torch.zeros((self.batch_size, self.hidden_size))]
         for t in range(max_len):
             """Retarget A to B"""       
-            ptA_in = seqA_[:, t:t+1, :]
+            ptA_in = seqA_[:, t, :]
 
-            _, statesA_AB= self.enc_gru(ptA_in, initial_state=statesA_AB)
+            statesA_AB= self.enc_gru(ptA_in, initial_state=statesA_AB)
             if t == 0:
-                ptB_in = torch.zeros(self.batch_size, 1, 3 * self.n_joints + 4)
+                ptB_in = torch.zeros(self.batch_size, 3 * self.n_joints + 4)
             else:
                 ptB_in = torch.cat([b_local[-1], b_global[-1]], dim=-1)
 
             ptcombined = torch.cat(
                 [skelB_[:, 0, 3:], ptB_in, statesA_AB[-1]], dim=1)
-            _, statesB_AB= self.enc_gru(ptcombined, statesB_AB)
+            statesB_AB= self.dec_gru(ptcombined, statesB_AB)
             angles_n_offset = self.fc(statesB_AB[-1])
             output_angles = torch.reshape(angles_n_offset[:, :-4],
                                         (self.batch_size, self.n_joints, 4))
@@ -73,29 +86,62 @@ class Generator(nn.Module):
             if training:
                 ptB_in = torch.cat([b_local[-1], b_global[-1]], dim=-1)
 
-                n= self.enc_gru(tf.expand_dims(ptB_in, 1), training=True, initial_state=statesB_BA)
-                statesB_BA=n[1:]
+                statesB_BA=self.enc_gru(ptB_in, initial_state=statesB_BA)
 
                 if t == 0:
-                ptA_in = tf.zeros([self.batch_size, 3 * self.n_joints + 4])
+                    ptA_in = torch.zeros((self.batch_size, 3 * self.n_joints + 4))
                 else:
-                ptA_in = tf.concat([a_local[-1], a_global[-1]], axis=-1)
+                    ptA_in = torch.concat([a_local[-1], a_global[-1]], dim=-1)
 
-                ptcombined = tf.concat(
-                    values=[skelA_[:, 0, 3:], ptA_in, statesB_BA[-1]], axis=1)
-                n = self.dec_gru(tf.expand_dims(ptcombined, 1), training=True, initial_state=statesA_BA)
-                statesA_BA=n[1:]
+                ptcombined = torch.concat(
+                    [skelA_[:, 0, 3:], ptA_in, statesB_BA[-1]], dim=1)
+                statesA_BA = self.dec_gru(ptcombined, initial_state=statesA_BA)
                 angles_n_offset = self.fc(statesA_BA[-1])
-                output_angles = tf.reshape(angles_n_offset[:, :-4],
-                                        [self.batch_size, self.n_joints, 4])
+                output_angles = torch.reshape(angles_n_offset[:, :-4],
+                                        (self.batch_size, self.n_joints, 4))
                 a_global.append(angles_n_offset[:, -4:])
                 a_quats.append(self.normalized(output_angles))
 
-                skel_in = tf.reshape(skelA_[:, 0, :], [self.batch_size, self.n_joints, 3])
+                skel_in = torch.reshape(skelA_[:, 0, :], (self.batch_size, self.n_joints, 3))
                 skel_in = skel_in * dstd + dmean
 
                 output = (self.fk.run(parents, skel_in, output_angles) - dmean) / dstd
-                output = tf.reshape(output, [self.batch_size, -1])
+                output = torch.reshape(output, (self.batch_size, -1))
                 a_local.append(output)
 
         return b_local, b_global, b_quats, a_local, a_global, a_quats
+
+    def normalized(self, angles):
+        lengths = torch.sqrt(torch.sum(torch.square(angles), axis=-1))
+        return angles / lengths[..., None]
+
+
+class Discriminator(nn.Module):
+    def __init__(self, C):
+        super(Discriminator, self).__init__()
+        self.norm1=nn.BatchNorm1d(64)
+        self.norm2=nn.BatchNorm1d(128)
+        self.norm3=nn.BatchNorm1d(256)
+        self.lrelu=nn.LeakyReLU()
+        self.conv1=nn.Conv1d(in_channels=C, out_channels=32, kernel_size=3, stride=1, 
+            padding=1, name="conv1d_h0")
+        self.conv2=nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1,
+            padding=1, name="conv1d_h1")
+        self.conv3=nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1,
+            padding=1, name="conv1d_h2")
+        self.conv4=nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1,
+            padding=1, name="conv1d_h3")
+        self.conv5=nn.Conv1d(in_channels=256, out_channels=1, kernel_size=3, stride=1,
+            name="logits", padding=0)
+
+    def forword(self, x, cond, p, training=False):
+        x=F.dropout(x,p, training=training)
+        h0=self.lrelu(self.conv1(x))
+        h1=self.lrelu(self.norm1(self.conv2(h0)))
+        h1 = torch.cat([h1, torch.tile(cond, (1, int(h1.shape[1]), 1))], dim=-1)
+        h2=self.lrelu2(self.norm2(self.conv3(h1)))
+        h2 = torch.cat([h2, torch.tile(cond, (1, int(h2.shape[1]), 1))], dim=-1)
+        h3=self.lrelu3(self.norm3(self.conv4(h2)))
+        h3 = torch.cat([h3, torch.tile(cond, (1, int(h3.shape[1]), 1))], dim=-1)
+        logits = self.conv5(h3)
+        return logits
