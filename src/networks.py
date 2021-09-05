9423fc8cf361ebe59cffa1d7f9422a81f5acf1df
Han jun <hhjjhh0425@naver.com>
2021-09-06 01:25:34 +0900

    model network

--- src/networks.py
+++ src/networks.py
@@ -117,25 +117,25 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, C):
+    def __init__(self, C, L):
         super(Discriminator, self).__init__()
         self.norm1=nn.BatchNorm1d(64)
         self.norm2=nn.BatchNorm1d(128)
         self.norm3=nn.BatchNorm1d(256)
         self.lrelu=nn.LeakyReLU()
-        self.conv1=nn.Conv1d(in_channels=C, out_channels=32, kernel_size=3, stride=1, 
-            padding=1, name="conv1d_h0")
-        self.conv2=nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1,
-            padding=1, name="conv1d_h1")
-        self.conv3=nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1,
-            padding=1, name="conv1d_h2")
-        self.conv4=nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1,
-            padding=1, name="conv1d_h3")
-        self.conv5=nn.Conv1d(in_channels=256, out_channels=1, kernel_size=3, stride=1,
+        self.conv1=nn.Conv1d(in_channels=C, out_channels=32, kernel_size=4, stride=2, 
+            padding=(L+2)/2, name="conv1d_h0")
+        self.conv2=nn.Conv1d(in_channels=32, out_channels=64, kernel_size=4, stride=2,
+            padding=(L+2)/2, name="conv1d_h1")
+        self.conv3=nn.Conv1d(in_channels=64, out_channels=128, kernel_size=4, stride=2,
+            padding=(L+2)/2, name="conv1d_h2")
+        self.conv4=nn.Conv1d(in_channels=128, out_channels=256, kernel_size=4, stride=2,
+            padding=(L+2)/2, name="conv1d_h3")
+        self.conv5=nn.Conv1d(in_channels=256, out_channels=1, kernel_size=4, stride=2,
             name="logits", padding=0)
 
-    def forword(self, x, cond, p, training=False):
-        x=F.dropout(x,p, training=training)
+    def forword(self, x, cond, kp, training=False):
+        x=F.dropout(x,1-kp, training=training)
         h0=self.lrelu(self.conv1(x))
         h1=self.lrelu(self.norm1(self.conv2(h0)))
         h1 = torch.cat([h1, torch.tile(cond, (1, int(h1.shape[1]), 1))], dim=-1)
